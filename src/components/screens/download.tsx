618c8bbd8cb7697bf91b58190c89d2294c5b3242
nyujuzer <sallairobert94@gmail.com>
2023-12-04 08:19:11 +0100

    Minor changes on download.css

--- src/components/screens/download.tsx
+++ src/components/screens/download.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import Card from "../elements/card";
 import Button from "@mui/material/Button";
+import "./download.css"
 
 function Download() {
   return (
@@ -9,16 +10,18 @@ function Download() {
       <div>
         <div className="row">
           <Card title={"Android"}>
-            <Button color="primary">
-            <Link to={"https://google.com"}>
+            <Button variant={"contained"} className="btn-yimn">
+            <Link className="clickable-text" to={"https://google.com"}>
               <h1 >Download From Play Store</h1>
             </Link>
             </Button>
           </Card>
           <Card title={"Iphone"}>
-            <Link to={"https://google.com"}>
+            <Button variant="contained" className="btn-yimn">
+            <Link className="clickable-text" to={"https://google.com"}>
               <h1 >Download From Play Store</h1>
             </Link>
+            </Button>
           </Card>
         </div>
       </div>
